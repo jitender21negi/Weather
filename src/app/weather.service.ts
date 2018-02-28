c494c73fd5f76a8105347f04d5eed74e508615f7
jitender21negi <36669069+jitender21negi@users.noreply.github.com>
2018-02-27 22:58:18 -0800

    Update weather.service.ts

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherService {
 
-  constructor(private _http: HttpClient) { }
-
-  dailyWeather(city: string) {    
-    return this._http.get("http://api.openweathermap.org/data/2.5/forecast?q="+city+"+&appid=83009a92a0f60be5e2f746464624abe8")
-      .map(result => result);      
+  constructor(private _http: HttpClient) {
   }
-  currentWeather(city: string) {    
-    return this._http.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=83009a92a0f60be5e2f746464624abe8")
-    .map(result => result);  
+
+  dailyWeather(city: string) {
+    return this._http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '+&appid=83009a92a0f60be5e2f746464624abe8')
+      .map(result => result);
+  }
+
+  currentWeather(city: string) {
+    return this._http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=83009a92a0f60be5e2f746464624abe8')
+      .map(result => result);
   }
 }
