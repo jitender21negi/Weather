1b9362c0b1c43932dbc98c9136e0ad51261ca1b3
jitender21negi <36669069+jitender21negi@users.noreply.github.com>
2018-02-27 22:56:18 -0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { WeatherComponent } from './weather/weather.component';
+import {WeatherComponent} from './weather/weather.component';
 
 const appRoutes: Routes = [
-    { path: '', loadChildren: '' },
-    { path: 'weather', component: WeatherComponent }
+  {path: '', loadChildren: ''},
+  {path: 'weather', component: WeatherComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 
